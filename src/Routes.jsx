3ffba683cc1b6f92fa813072a09f48de966a76f1
agr3w <suzanakampa.sk@gmail.com>
2025-08-31 15:08:52 -0300

    Componentizando as paginas de conteudo

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ContentPageDocumentaries from "./pages/ContentPageDocumentaries";
-import ContentPageQuizzes from "./pages/ContentPageQuizzes";
-
+import { quizzesData } from "./content/contentGrid/quizzesContent";
+import { documentariesData } from "./content/contentGrid/documentariesContent";
+import ContentPageTemplate from "./pages/ContentPageTemplate";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/documentaries" element={<ContentPageDocumentaries />} />
-        <Route path="/quizzes" element={<ContentPageQuizzes />} />
+        <Route
+          path="/documentaries"
+          element={
+            <ContentPageTemplate
+              data={documentariesData}
+              title="Nossos Documentários"
+              description="Assista a documentários incríveis e aprenda sobre a vida marinha e a história dos oceanos."
+            />
+          }
+        />
+        <Route
+          exact
+          path="/quizzes"
+          element={
+            <ContentPageTemplate
+              data={quizzesData}
+              title="Nossos Quizzes"
+              description="Teste seus conhecimentos sobre a vida marinha e a história dos oceanos."
+            />
+          }
+        ></Route>
       </Routes>
     </Router>
   );
